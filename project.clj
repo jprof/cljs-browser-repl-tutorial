da33305542ed83b1d26b033883b5b62d6473b588
Justin Proffitt <justindproffitt@gmail.com>
2013-11-22 00:39:43 -0500

    Add dependencies and plugins to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2030"]]
+  :plugins [[lein-cljsbuild "1.0.0"]]
+  :cljsbuild {:builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "js/main.js"
+                                   :output-dir "out"
+                                   :pretty-print true}}]})
